a5823a9490aaf004dc8c9c3ac3d2f8d8a1b21e40
yiseoho95 <yiseoho95@gmail.com>
2021-03-29 13:25:59 -0400

    movieshop(inclass)

---
+++ MovieShop1/MovieShop.MVC/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // It will look fora aView with a name called Index( because the action method name is index)
+            return View("TestView");
+        }
+    }
+}
